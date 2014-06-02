252ef3b189b750b9d3a5a425977be3bcf142f6a1
Trevor <ubpd34th@gmail.com>
2014-06-02 08:46:58 -0400

    Removed IP
    a whoops

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var utils = require("./utils");
-const LOCAL_IP = "207.12.89.250"; // ************* Change to the IP address of the machine you are running the DNS proxy and Web server on *************
+const LOCAL_IP = ""; // ************* Change to the IP address of the machine you are running the DNS proxy and Web server on *************
 
 console.log("Starting GTA-DNS - Let the hacking begin :)");
 utils.startDnsProxy(LOCAL_IP);
